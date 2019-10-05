9b8eba9dfae1690013cf75db817b1cda8e8e6bb6
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2019-10-05 15:32:29 +0800

    Update test2词袋.py

--- nlp/task1/test2词袋.py
+++ nlp/task1/test2词袋.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import os
 
 path = './sentiment labelled sentences'
 data = []
@@ -22,10 +21,11 @@ for in_ in sentense:
             for z in y.split('.'):
                 words.append(z)
 words = set(words)
+'''
 for word in words:
     print(word)
 print(len(words))
-
+'''
 for se in sentense:
     tmp = []
     for word in words:
@@ -43,13 +43,15 @@ id = np.arange(1000)
 np.random.shuffle(id)
 test_data = []
 test_label = []
-for i in id:
-    if i < 800:
-        if str(label[i]) == 0:
+
+for j, i in enumerate(id):
+    if j < 800:
+        if int(label[i]) == 0:
             train_label.append([1, 0])
+            train_data.append(data[i])
         else:
             train_label.append([0, 1])
-        train_data.append(data[i])
+            train_data.append(data[i])
     else:
         if str(label[i]) == 0:
             test_label.append([1, 0])
@@ -72,23 +74,65 @@ y = tf.sigmoid(out)
 
 cross_entropy = - y_ * tf.log(tf.clip_by_value(y, 1e-10, 1))
 loss = tf.reduce_sum(cross_entropy)
-optimizer = tf.train.AdamOptimizer(0.005)
+optimizer = tf.train.AdamOptimizer(0.15)
 train = optimizer.minimize(loss)
 
 predict = tf.equal(tf.argmax(y_, 1), tf.argmax(y, 1))
-acc = tf.reduce_mean(tf.cast(predict, "float"))
+acc = tf.reduce_mean(tf.cast(predict, tf.float32))
 
 init = tf.global_variables_initializer()
-batch_size = 200
-batch_num = 5
+batch_size = 50
+batch_num = 20
+
+tf.summary.scalar('accuracy', acc)
+merge_summary = tf.summary.merge_all()
 
 with tf.Session() as sess:
+    writer = tf.summary.FileWriter('ret/', sess.graph)
     sess.run(init)
     for step in range(500):
         for batch in range(batch_num):
             x_s = train_data[batch * batch_size: (batch + 1) * batch_size]
             y_s = train_label[batch * batch_size: (batch + 1) * batch_size]
             sess.run(train, feed_dict={x: x_s, y_: y_s})
+
+        train_summary = sess.run(merge_summary, feed_dict={x: test_data, y_: test_label})
+        writer.add_summary(train_summary, step)
         if step % 50 == 0:
             print('step', step, 'loss', sess.run(loss, feed_dict={x: test_data, y_: test_label}),
                   'accuracy', sess.run(acc, feed_dict={x: test_data, y_: test_label}))
+'''
+    # input test period
+    print('start input test')
+    while True:
+        line = input()
+        answer = int(input())
+        print(line)
+        print(answer)
+        ans = []
+        # 1: like
+        # 0: not like
+        if answer == 0:
+            ans.append([1, 0])
+        elif answer == 1:
+            ans.append([0, 1])
+        else:
+            break
+        ans = np.array(ans)
+        tmp = []
+        prep = []
+        for xx in line.split():
+            for yy in xx.split(','):
+                for z in yy.split('.'):
+                    prep.append(z)
+        for word in words:
+            if word in prep:
+                tmp.append(1)
+                print('find', word)
+            else:
+                tmp.append(0)
+        x_in = np.array([tmp])
+        print('x_in', x_in, 'ans', ans)
+        print(x_in.shape, ans.shape)
+        print(sess.run(predict, feed_dict={x: x_in, y_: ans}))
+'''
