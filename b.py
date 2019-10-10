0baa772e677d1c0cef6f5bee2e88536afb4b6792
liuchengyuan123 <34617968+liuchengyuan123@users.noreply.github.com>
2019-10-10 09:42:27 +0800

    Update b.py

--- b.py
+++ b.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Dense:
+class Output_layer:
 
     def __init__(self, in_dim, out_dim):
         m, self.n = in_dim
@@ -18,7 +18,7 @@ class Dense:
         return self.predict
 
     def get_grad(self, data_in, predict, label):
-        error = - (predict - label)
+        error = predict - label
         grad_w = []
         for i in range(self.n):
             grad_w.append([np.mean(error * data_in[:, i][:, np.newaxis])])
@@ -27,8 +27,8 @@ class Dense:
     
     def backward(self, data_in, label, alpha):
         grad_w, grad_b = self.get_grad(data_in, self.predict, label)
-        self.w += alpha * grad_w
-        self.b += alpha * grad_b
+        self.w -= alpha * grad_w
+        self.b -= alpha * grad_b
 
     def train(self, data_in, label, alpha=0.2):
         self.forward(data_in)
@@ -37,6 +37,66 @@ class Dense:
     def get_loss(self, predict, label):
         return -np.mean(label * np.log(np.clip(predict, 1e-10, 1)) + (1 - label) * np.log(np.clip(1 - predict, 1e-10, 1)))
 
+class Layer:
+
+    def __init__(self, in_dim, out_dim):
+        m, self.n = in_dim
+        om, self.on = out_dim
+        self.w = np.random.rand(self.n, self.on)
+        self.b = np.random.zeros([1, self.on])
+        self.predict = None
+
+    def softmax(self, z):
+        ss = np.sum(np.exp(z))
+        return np.exp(z) / ss
+        
+    def forward(self, data_in):
+        self.predict = self.softmax(np.matmul(data_in, self.w) + self.b)
+        return self.predict
+    
+    def get_grad(self, data_in, predict, label):
+        error = []
+        m = len(predict)
+        for i in range(len(self.predict)):
+            tmp = []
+            for j in range(self.on):
+                t = []
+                for k in range(self.on):
+                    if j == k:
+                        t.append(- label[i][k] * (1 - predict[i][j]))
+                    else:
+                        t.append(- label[i][j] * predict[i][k])
+                tmp.append(np.sum(t))
+            error.append(tmp)
+        error = np.array(error)
+        grad_w = []
+        for i in range(self.n):
+            tmp = []
+            for j in range(self.on):
+                lc = error[j:, ] * data_in[i:, ]
+                tmp.append(np.mean(lc))
+            grad_w.append(tmp)
+        grad_b = []
+        for j in range(self.on):
+            grad_b.append(np.mean(error[j:, ]))
+        return np.array(grad_w), np.array(grad_b)
+    
+    def backward(self, data_in, label, alpha=0.2):
+        grad_w, grad_b = self.get_grad(data_in, self.predict, label)
+        self.w -= alpha * grad_w
+        self.b -= alpha * grad_b
+
+    def train(self, data_in, label, alpha=0.2):
+        self.forward(data_in)
+        self.backward(data_in, label, alpha)
+    
+    def get_acc(self, label):
+        return np.mean(np.cast(np.equal(np.argmax(label, 1), np.argmax(self.predict, 1)), np.float))
+    
+    def get_loss(self, label):
+        return np.mean(np.sum(- label * np.log(np.clip(self.predict, 1e-10, 1)), 1))
+
+'''
 data_in = 10 * np.random.rand(500, 2)
 label = []
 for d in data_in:
@@ -67,3 +127,4 @@ plt.figure()
 plt.scatter(p1x, p1y)
 plt.scatter(p2x, p2y)
 plt.show()
+'''
